3b186e215ae17b3afa3787763530952218aeecbc
Ondra58 <97514734+Ondra58@users.noreply.github.com>
2023-11-22 11:22:21 +0100

    Add files via upload

---
+++ RetezUtil/RetezUtil/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace RetezUtil
+{
+    static public class Retezec
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        static public string Smaz(string text)
+        {
+            foreach(char znak in text)
+            {
+                if(znak >= '0' && znak <= '9')
+                {
+                    text = text.Remove(znak);
+                }
+            }
+            return text;
+        }
+    }
+}
